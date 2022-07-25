f3be86c75a1ce18bf7531977248026e6e0b10843
Zeno Sbardelotto Junior <zeno.sbardelotto@universo.univates.br>
2022-07-24 22:51:51 -0300

    Support to resume with many pages (mobile/desktop)

--- components/resume/A4.vue
+++ components/resume/A4.vue
@@ -1,33 +1,40 @@
 <script setup>
 const { options } = defineProps(['options'])
-const paper = ref(null)
 const a4 = ref(null)
+const print = ref(null)
 
-const { isFullscreen, exit, toggle } = useFullscreen(a4)
-
+// Fullscreen
+const { isFullscreen, exit, toggle } = useFullscreen(print)
 watch(() => options.fullscreen.enable, () => toggle())
-onClickOutside(paper, (event) => exit())
+onClickOutside(a4, (event) => exit())
+
+// Hack when only one page to print. Won't show the second empty page
+onMounted(() => {
+  const { load, unload } = useStyleTag('body, html { overflow: hidden }', { media: 'print' })
+  const pages = a4.value.getElementsByClassName('page').length;
+  (pages === 1) ? load() : unload();
+})
 </script>
 
 <template>
-  <section id="print" ref="a4" class="flex justify-center md:mb-20">
-    <div ref="paper" :class="['transform', { isFullscreen }]">
-      <div
-        class="paper"
-        :style="{
-          background: `url('a4-texture.png') #fff`,
-        }"
-      >
-        <slot />
-      </div>
+  <section id="print" ref="print" class="flex justify-center md:mb-20">
+    <div ref="a4" :class="['transform', { isFullscreen }]">
+      <slot />
     </div>
   </section>
 </template>
 
+<style lang="scss" print>
+  .page { page-break-after: always; }
+  @page { size: A4; margin: 0; }
+  body, html { height: 100%; margin: 0 !important; }
+</style>
+
 <style lang="scss">
-.paper {
+.page {
   width: 21cm!important;
   height: 29.7cm!important;
+  background: url('a4-texture.png') #fff;
   *::selection {
     background: #283441;
     color: #fff;
@@ -40,62 +47,55 @@ onClickOutside(paper, (event) => exit())
       transform: scale(0.5);
       margin: -150px -20rem;
       overflow: hidden;
-      &:not(.isFullscreen) {
-        height: 700px;
-      }
+      &:not(.isFullscreen) { height: 700px; }
     }
-    .paper {
+    .page {
       align-self: flex-start;
     }
   }
 }
-
-@media print {
-  @page { margin: 0; }
-  body { margin: 0; }
-}
 </style>
 
 <style lang="postcss" scoped>
-.paper {
-  @apply flex gap-10 p-16 cursor-text;
+#print:deep(.page) {
+  @apply flex gap-10 p-16 cursor-text md:mb-4;
 }
-.paper:deep(section) {
+#print:deep(.page section) {
   @apply w-4/6;
 }
 
-.paper:deep(aside) {
+#print:deep(.page aside) {
   @apply w-2/6;
 }
-.paper:deep(h1) {
+#print:deep(.page h1) {
   color: #666666;
   @apply font-semibold mb-3 font-roboto text-5xl leading-6 mb-6;
 }
-.paper:deep(hr) {
+#print:deep(.page hr) {
   @apply my-5 border-t-gray-300;
 }
 
-.paper:deep(h2) {
+#print:deep(.page h2) {
   font-size: 16px;
   color: #000;
   @apply text-regular font-roboto text-gray-500 leading-5;
 }
-.paper:deep(h4) {
+#print:deep(.page h4) {
   @apply font-bold font-sans text-gray-600 text-tiny leading-6;
 }
 
-.paper:deep(ol)::before, .paper:deep(article)::before {
+#print:deep(.page ol)::before, #print:deep(.page article)::before {
   content: attr(data-title);
-  @apply block uppercase cursor-text font-roboto font-light text-tiny mb-1 print:text-picton-blue-700;
+  @apply block uppercase cursor-text font-roboto font-light text-tiny mb-1 text-gray-900 print:text-picton-blue-700;
 }
-.paper:deep(ol > li), .paper:deep(p) {
+#print:deep(.page ol > li), #print:deep(.page p), #print:deep(.page span) {
   @apply text-tiny font-roboto text-gray-500 text-justify leading-4;
 }
 
-.paper:deep(ol) {
+#print:deep(.page ol) {
   @apply mb-5;
 }
-.paper:deep(ol:first-of-type) {
+#print:deep(.page ol:first-of-type) {
   @apply mt-5;
 }
 </style>
